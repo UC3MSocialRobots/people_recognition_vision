9f2ff18f99b8acf244df077176de81654eadceab
Arnaud Ramey <arnaud.a.ramey@gmail.com>
2015-03-24 10:53:09 +0000

    replacing hard coded groovy with rosversion

--- scripts/rosdep.sh
+++ scripts/rosdep.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # External, ROS and system package dependencies
 
-PACKAGES="ros-hydro-openni-camera
-          ros-hydro-openni-tracker
-          ros-hydro-openni-launch"
+PACKAGES="ros-`rosversion -d`-openni-camera
+          ros-`rosversion -d`-openni-tracker
+          ros-`rosversion -d`-openni-launch"
 
 sudo apt-get install $PACKAGES
