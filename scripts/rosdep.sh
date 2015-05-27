6546df1a960ff983bc523e15f7fee65343a6e8c0
Raúl Pérula-Martínez <raules@gmail.com>
2015-05-27 10:11:07 +0000

    updated deps to install in package.xml

--- scripts/rosdep.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# External, ROS and system package dependencies
-
-PACKAGES="ros-`rosversion -d`-openni-camera
-          ros-`rosversion -d`-openni-tracker
-          ros-`rosversion -d`-openni-launch"
-
-sudo apt-get install $PACKAGES
