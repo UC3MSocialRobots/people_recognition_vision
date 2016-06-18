22526715ec94504e0e52b747e4f501f5c0bcc571
Arnaud Ramey <arnaud.a.ramey@gmail.com>
2016-06-18 11:58:43 +0200

    writing the doc

---
+++ scripts/rosdep.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# External, ROS and system package dependencies for the "people_recognition_vision" package
+
+PACKAGES="libblas-dev
+          libgsl0-dev"
+
+sudo apt-get install $PACKAGES
